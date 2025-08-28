a77e8fca82427cbae08efa9b6a6752fcd3002063
slappyburger <micha.keiten002@stud.fh-dortmund.de>
2025-08-28 11:17:54 +0200

    Add: Backend, Basic Controller, Services and DTOs
    Testing Endpoint /exercises/records (get request)

---
+++ backend/backend/src/main/java/com/fitnesstracker/backend/dtos/ExerciseRecordDto.java
@@ -0,0 +1,17 @@
+package com.fitnesstracker.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ExerciseRecordDto {
+    private Long id;
+    private String exercise;
+    private Integer weight;
+    private Integer repeats;
+}
